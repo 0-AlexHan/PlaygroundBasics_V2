4889efc956e2886d01509760abb78db8a0c99ae2
Alexhan <alexandrganin0@gmail.com>
2021-07-02 13:35:35 +0300

    Protocols

--- PlaygroundBasics_V2.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ PlaygroundBasics_V2.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -16,6 +16,12 @@ import Foundation
  */
 // Добавь код сюда:
 
+protocol Employee {
+    var hours: Int { get set }
+    
+    func work()
+}
+
 /*:
  - Обьяви два класса _Painter_ и _Doctor_.
  - Укажи, что оба этих класса удовлетворяют протоколу.
@@ -23,11 +29,32 @@ import Foundation
  */
 // Добавь код сюда:
 
+class Painter: Employee {
+    var hours: Int = 17520
+    
+    func work() {
+        print("Painter was drowing his square for about \(hours) hours")
+    }
+    
+}
+
+class Doctor: Employee {
+    var hours: Int = 12
+    
+    func work() {
+        print("Doc has worked \(hours) hours")
+    }
+    
+}
 /*:
  - Создай экземпляры каждого из классов.
  - Вызови метод _work_ для каждого из них.
  */
+let painter = Painter()
+let doc = Doctor()
 
+painter.work()
+doc.work()
 /*:
 ---
 #### Задание 2
@@ -36,5 +63,77 @@ import Foundation
  - Note: 👆 _Обрати внимание, что классы iOS, androidOS  и windowsOS удовлетворяют протоколу Internetable_
  */
 // Добавь код сюда:
+
+protocol Telephone {
+    func makeCall()
+    func hangUp()
+}
+
+protocol Landline: Telephone {
+    
+}
+
+protocol Cellular: Telephone {
+    func sendSMS()
+}
+
+protocol Rotaryable {
+    func rotaryInput()
+}
+
+protocol PushButtonable {
+    func buttonInput()
+}
+
+protocol Touchable {
+    func touchInput()
+}
+
+protocol Internetable {
+    func accessInternet()
+}
+
+class Push_button: Landline, PushButtonable {
+    func buttonInput() {}
+    func makeCall() {}
+    func hangUp() {}
+}
+
+class Rotary: Landline, Rotaryable {
+    func makeCall() {}
+    func hangUp() {}
+    func rotaryInput() {}
+}
+
+class Non_smart: Cellular, PushButtonable {
+    func sendSMS() {}
+    func buttonInput() {}
+    func makeCall() {}
+    func hangUp() {}
+}
+
+class iPhone: Cellular, Touchable, Internetable {
+    func sendSMS() {}
+    func touchInput() {}
+    func accessInternet() {}
+    func makeCall() {}
+    func hangUp() {}
+}
+class Androidphone: Cellular, Touchable, Internetable {
+    func sendSMS() {}
+    func touchInput() {}
+    func accessInternet() {}
+    func makeCall() {}
+    func hangUp() {}
+}
+
+class Windosphone: Cellular, Touchable, Internetable {
+    func sendSMS() {}
+    func touchInput() {}
+    func accessInternet() {}
+    func makeCall() {}
+    func hangUp() {}
+}
+
 
 //: [Назад: Структуры и классы](@previous)  |  Страница 11  |  [Вперед: Делегаты](@next)
